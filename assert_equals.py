efda88d851c1a04aab19fafa44583529b71890c9
Lee Staples <lmstaples94@gmail.com>
2021-06-24 22:00:20 +0200

    Complete everything

--- assert_equals.py
+++ assert_equals.py
@@ -1,2 +1,23 @@
 def assert_equals(expected, actual):
-    raise Exception("Expected 1 but found 0")
+    _assert_equals(expected, actual)
+
+    if type(expected) == list:
+        if len(expected) != len(actual):
+            raise Exception(f"Expected list length {len(expected)} but found {len(actual)}")
+        else:
+            for i in range(len(expected)):
+                _assert_equals(expected[i], actual[i])
+
+
+def _assert_equals(expected, actual):
+    expected_type = type(expected)
+    actual_type = type(actual)
+
+    if expected_type != actual_type:
+        raise Exception(f"Expected type {expected_type.__name__} but found type {actual_type.__name__}")
+    if expected_type == str:
+        if expected != actual:
+            raise Exception(f"Expected \"{expected}\" but found \"{actual}\"")
+    elif expected_type == int:
+        if expected != actual:
+            raise Exception(f"Expected {expected} but found {actual}")
--- test_assert_equals.py
+++ test_assert_equals.py
@@ -3,6 +3,43 @@ import pytest
 from assert_equals import assert_equals
 
 
-def test_first_scenario():
-    with pytest.raises(Exception, match="Expected 1 but found 0"):
-        assert_equals(1, 0)
+def test_no_error_string():
+    assert_equals("abc", "abc")
+
+
+def test_error_string():
+    with pytest.raises(Exception, match="Expected \"abcef\" but found \"abc\""):
+        assert_equals("abcef", "abc")
+
+
+def test_no_error_integer():
+    assert_equals(1, 1)
+
+
+def test_error_integer():
+    with pytest.raises(Exception, match="Expected 1 but found 2"):
+        assert_equals(1, 2)
+
+
+def test_error_type_coercion():
+    with pytest.raises(Exception, match="Expected type int but found type str"):
+        assert_equals(1, '1')
+
+
+def test_no_error_list():
+    assert_equals(["a", "b", "c"], ["a", "b", "c"])
+
+
+def test_error_list_length():
+    with pytest.raises(Exception, match="Expected list length 2 but found 3"):
+        assert_equals(["a", "b"], ["a", "b", "c"])
+
+
+def test_error_list_items_string():
+    with pytest.raises(Exception, match="Expected \"b\" but found \"d\""):
+        assert_equals(["a", "b"], ["a", "d"])
+
+
+def test_error_list_items_int():
+    with pytest.raises(Exception, match="Expected 2 but found 3"):
+        assert_equals([1, 2], [1, 3])
